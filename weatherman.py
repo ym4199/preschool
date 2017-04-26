ee8f462c7ea17022ed737275e851fa8295e5ff2b
ym4199 <ym4199@gmail.com>
2017-04-26 11:17:08 +0900

    practice

---
+++ weatherman.py
@@ -0,0 +1,10 @@
+import report
+
+description = report.get_description()
+print("Today's weather:", description)
+
+
+
+#import report as wr """Using alias"""
+#description = wr.get_description()
+#print("Today's weather:", description)
